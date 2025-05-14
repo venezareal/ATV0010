5e28c510d3134c5116f9adf79b57d436b9845487
Vanessa <venezareal@gmail.com>
2025-05-13 22:35:00 -0300

    Digitando os dados demográficos de uma cidade e salvando-os em variáveis.

--- Zuplae.Aulas.Atv0010/Zuplae.Aulas.Atv0010.ConsoleApp/Program.cs
+++ Zuplae.Aulas.Atv0010/Zuplae.Aulas.Atv0010.ConsoleApp/Program.cs
@@ -11,6 +11,39 @@ namespace Zuplae.Aulas.Atv0010.ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("\n\t========== ATIVIDADE 10 ==========");
+
+            Console.Write("\tDigite o nome da cidade: ");
+            String nomecid = Console.ReadLine();
+
+            Console.Write("\n\tDigite a extensão territorial: ");
+            double territorio = Console.ReadLine();
+
+            Console.Write("\n\tDigite a população em habitantes: ");
+            Int64 habitantes = Console.ReadLine();
+
+            Console.Write("\n\tDigite a densidade demográfica - Hab/Km²: ");
+            double densidade = Console.ReadLine();
+
+            Console.Write("\n\tDigite o PIB per capita: ");
+            double pib = Console.ReadLine();
+
+            Console.Write("\n\tDigite a porcentagem de escolarização - 6 a 14 anos:");
+            double escolaridade = Console.ReadLine();
+
+            Console.Write("\n\tDigite a taxa de analfabetismo em indivíduos +15 anos: ");
+            double analfabetismo = Console.ReadLine();
+
+            Console.Write("\n\tDigite a taxa de fecundidade: ");
+            double fecundidade = Console.ReadLine();
+
+            Console.Write("\n\tDigite a taxa de mortalidade infantil: ");
+            double mortalidadeInf = Console.ReadLine();
+
+            Console.Write("\n\tDigite a taxa de desocupação: ");
+            double desocupacao = Console.ReadLine();
+
+            Console.Write("\n\tDigite a taxa IPCA mensal:\n");
+            double ipca = Console.ReadLine();
         }
     }
 }
