2329c9127861444a252ada23742451cc0b3b5d39
Vanessa <venezareal@gmail.com>
2025-05-14 23:14:45 -0300

    Alterando os tipos de variáveis usando C#.

--- Zuplae.Aulas.Atv0010/Zuplae.Aulas.Atv0010.ConsoleApp/Program.cs
+++ Zuplae.Aulas.Atv0010/Zuplae.Aulas.Atv0010.ConsoleApp/Program.cs
@@ -16,34 +16,16 @@ namespace Zuplae.Aulas.Atv0010.ConsoleApp
             String nomecid = Console.ReadLine();
 
             Console.Write("\n\tDigite a extensão territorial: ");
-            double territorio = Console.ReadLine();
+            float territorio = Convert.ToSingle(Console.ReadLine());
 
             Console.Write("\n\tDigite a população em habitantes: ");
-            Int64 habitantes = Console.ReadLine();
+            int habitantes = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("\n\tDigite a densidade demográfica - Hab/Km²: ");
-            double densidade = Console.ReadLine();
+            Console.Write("\n\tDigite o PIB: ");
+            float pib = Convert.ToSingle(Console.ReadLine());
 
-            Console.Write("\n\tDigite o PIB per capita: ");
-            double pib = Console.ReadLine();
-
-            Console.Write("\n\tDigite a porcentagem de escolarização - 6 a 14 anos:");
-            double escolaridade = Console.ReadLine();
-
-            Console.Write("\n\tDigite a taxa de analfabetismo em indivíduos +15 anos: ");
-            double analfabetismo = Console.ReadLine();
-
-            Console.Write("\n\tDigite a taxa de fecundidade: ");
-            double fecundidade = Console.ReadLine();
-
-            Console.Write("\n\tDigite a taxa de mortalidade infantil: ");
-            double mortalidadeInf = Console.ReadLine();
-
-            Console.Write("\n\tDigite a taxa de desocupação: ");
-            double desocupacao = Console.ReadLine();
-
-            Console.Write("\n\tDigite a taxa IPCA mensal:\n");
-            double ipca = Console.ReadLine();
+           
+            float pibPerCapita = pib / habitantes;
 
 
             Console.WriteLine("\n\n");
@@ -52,13 +34,8 @@ namespace Zuplae.Aulas.Atv0010.ConsoleApp
             Console.Write($"\n\tNome da Cidade: {nomecid}");
             Console.Write($"\n\tArea Territorial: {territorio}");
             Console.Write($"\n\tNúmero de Habitantes: {habitantes}");
-            Console.Write($"\n\tDensidade Demográfica: {densidade}");
             Console.Write($"\n\tPIB Per Capita: {pib}");
-            Console.Write($"\n\tEscolarização 6-14 anos: {escolaridade}");
-            Console.Write($"\n\tAnalfabetismo + 15 anos: {analfabetismo}");
-            Console.Write($"\n\tQuantidade Filhos por mulher: {fecundidade}");
-            Console.Write($"\n\tTaxa Mortalidade Infantil: {mortalidadeInf}");
-            Console.Write($"\n\tTaxa de Desocupação: {desocupacao}\n");
+            Console.Write($"\n\n\n\tPib Per Capita: {pibPerCapita}\n");
         }
     }
 }
